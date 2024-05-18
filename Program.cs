832bd1a3a4b8b6aa5bd58027f9fec0d7d8c17320
viktorslany <viktor.slany@hotmail.com>
2024-05-18 22:02:19 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyDotNetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Number Guessing Game!");
+            Console.WriteLine("Guess number between 1 and 100.");
+            Random random = new Random();
+            int secretNumber = random.Next(1, 101);
+            int attempts = 0;
+            bool guessedCorrectly = false;
+
+            while (!guessedCorrectly)
+            {
+                Console.Write("Enter your number: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int guess))
+                {
+                    attempts++;
+
+                    if (guess == secretNumber)
+                    {
+                        guessedCorrectly = true;
+                        Console.WriteLine($"Congratulations! You guessed the number in {attempts} attempts.");
+                    }
+                    else if (guess < secretNumber)
+                    {
+                        Console.WriteLine("Too low! Try a higher number.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too high! Try a lower number.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+        }
+    }
+}
